fa34b866d8abf13b86a6331e6c41897915b766cf
Piotr Borysewicz <piotrborysewicz@yahoo.co.uk>
2022-11-30 14:01:45 +0000

    new

---
+++ script.js
@@ -0,0 +1,20 @@
+myData = []
+
+let first = document.getElementById('firstName').value
+let last = document.getElementById('lastName').value
+
+function myFoo(event) {
+    event.preventDefault()
+    let myDictionary = {firstName:"", lastName:""}
+    myDictionary.firstName=first
+    myDictionary.lastName=last
+    myData.push(myDictionary)
+    window.alert(myData)
+    
+
+    }
+
+function create() {
+    document.getElementById('nameCV').innerText=myData[0].firstName
+    document.getElementById('lastCV').innerText=myData[0].lastName
+}
